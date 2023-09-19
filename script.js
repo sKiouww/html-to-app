3fd5dd468b86a9a4484ed7b9b8ede0a45493871b
Lois Rabergeau <lois.rabergeau@etu.univ-poitiers.fr>
2023-09-19 18:09:15 +0200

    ajout sous titre

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ function changeTitle() {
     title.innerText += " from JS";
     title.style.color = 'red';
 }
+
+const h2 = document.createElement("h2");
+h2.id = "sous-titre";
+h2.innerText = "Welcome to the javascript world";
+document.querySelector("h1").after(h2);
